15c35f88b7a37800ad78da0777db8bf39d4493ef
xioperez01 <angie.xioperez@gmail.com>
2022-04-23 11:01:43 -0500

    Fix(MainSection): Fix MainSection position

--- src/MainSection/MainSection.jsx
+++ src/MainSection/MainSection.jsx
@@ -7,9 +7,9 @@ const MainSecction = () => {
       w="calc(100% - 240px)"
       bgColor="#1D1D1D"
       position="absolute"
-      top="60px"
+      top={0}
       right={0}
-      h="calc(100% - (60px + 90px))"
+      h="calc(100% -  90px)"
       overflowY="auto"
       as="section"
     ></Box>
