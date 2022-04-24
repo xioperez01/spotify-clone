5402721e4dcc06615cd9ca7adb72a8e6d96830db
xioperez01 <angie.xioperez@gmail.com>
2022-04-24 16:42:05 -0500

    feat(HomeView): Implememt first grid

--- src/HomeView/HomeView.jsx
+++ src/HomeView/HomeView.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Box, Grid, Heading } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Heading } from "@chakra-ui/react";
 import { useDataLayerValue } from "../DataLayer";
 import CardType1 from "../Shared/Cards/CardType1";
+import { colorGenerate } from "../Shared/Functions/changeBgColor";
 
 const HomeView = () => {
   const [{ playlists }] = useDataLayerValue();
-  console.log(playlists);
+  const [color, setColor] = React.useState("");
+
+  const handleChangeColor = (color) => {
+    setColor(colorGenerate(color));
+  };
 
   const currentHour = new Date().getHours();
-  console.log(currentHour);
   return (
     <Box
       w="calc(100% - 240px)"
@@ -18,25 +22,45 @@ const HomeView = () => {
       right={0}
       h="calc(100% -  90px)"
       overflowY="auto"
+      overflowX="hidden"
       as="section"
-      pt="80px"
-      px={8}
     >
-      <Heading color="white" fontSize="32px">
-        {currentHour <= 12 ? "¡Buenos dias!" : ""}
-      </Heading>
-      <Grid
-        w="100%"
-        h="auto"
-        mt={8}
-        templateColumns='repeat(3, 1fr)'
-        templateRows='repeat(2, 1fr)'
-        gap={4}
+      <Box
+        pt="80px"
+        px={{ base: 4, lg: 8 }}
+        bg={color}
+        transition="background 2s"
       >
-        {playlists?.items?.map((list, index) =>
-          index <= 5 ? <CardType1 key={list?.id} item={list} /> : <></>
-        )}
-      </Grid>
+        <Heading color="white" fontSize="32px">
+          {currentHour <= 12
+            ? "¡Buenos días!"
+            : currentHour > 12 && currentHour <= 18
+            ? "¡Buenas tardes!"
+            : "¡Buenas noches!"}
+        </Heading>
+        <Grid
+          w="100%"
+          h="auto"
+          mt={6}
+          templateColumns={{ base: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
+          templateRows={{ base: "repeat(3, 1fr)", lg: "repeat(2, 1fr)" }}
+          gap={4}
+        >
+          <GridItem>
+            <CardType1 onChangeColor={handleChangeColor} />
+          </GridItem>
+
+          {playlists?.items?.map((list, index) =>
+            index <= 4 ? (
+              <GridItem key={list?.id}>
+                <CardType1 item={list} onChangeColor={handleChangeColor} />
+              </GridItem>
+            ) : (
+              <></>
+            )
+          )}
+        </Grid>
+      </Box>
     </Box>
   );
 };
